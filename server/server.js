e0d391880f4248e6bef51254cebc87940653dff5
Josh Ivan Lanuevo <lanuevo68@gmail.com>
2023-12-23 14:14:58 +0800

    server setup.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 5000;
 
+app.use(cors()); 
+
 app.use(express.json());
+
+app.post("/api/calculate", (req, res) => {
+  const { expression } = req.body;
+  const result = evaluateExpression(expression);
+  res.json({ result });
+}); 
+
+function evaluateExpression(expression) {
+  // Implement your own logic to evaluate the expression without using eval()
+  // You might want to use a library or implement parsing and calculation
+  // This is just a placeholder
+  return eval(expression);
+}
+
+app.all('*', (req, res) => {
+  res.status(404).json({ error: 'Not Found' });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
